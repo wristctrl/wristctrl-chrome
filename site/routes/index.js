773101c254e5d86f8864a1df6c5413e50d86b600
Rhys Howell <howell33@purdue.edu>
2015-01-16 21:10:54 -0500

    Started basic iframe page

--- site/routes/index.js
+++ site/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Wrist Control' });
+router.get('/*', function(req, res, next) {
+  var requestedURL = req.params[0];
+
+  //console.log("Here's the requested extension: " + requestedURL);
+  res.render('index', { title: 'Wrist Control',urlToFrame: requestedURL });
 });
 
 module.exports = router;
