0a1b2a00aa1b491ce2892b568906cc7413c96174
Rhys Howell <howell33@purdue.edu>
2015-01-16 21:23:56 -0500

    Added a frame jade page for when someone connects with a frame in the url

--- site/routes/index.js
+++ site/routes/index.js
@@ -2,11 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/*', function(req, res, next) {
-  var requestedURL = req.params[0];
+router.get('/', function(req, res, next) {
+  //console.log("HOME PAGE REQUESTED");
 
-  //console.log("Here's the requested extension: " + requestedURL);
-  res.render('index', { title: 'Wrist Control',urlToFrame: requestedURL });
+  res.render('index', { title: 'Wrist Control'});
+});
+
+/* GET some url */
+router.get('/*', function(req, res, next) {
+    var requestedURL = req.params[0];
+
+    //console.log("Here's the requested extension: " + requestedURL);
+    res.render('frame', { title: 'Wrist Control',urlToFrame: requestedURL });
 });
 
 module.exports = router;
