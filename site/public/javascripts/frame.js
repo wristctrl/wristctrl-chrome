d66f2727ab717b2870bbe2d5bb5b465ab9babbc9
Kirby Kohlmorgen <kirby@purdue.edu>
2015-01-18 06:22:34 -0500

    Add from market functionality

--- site/public/javascripts/frame.js
+++ site/public/javascripts/frame.js
@@ -3,8 +3,25 @@ var app = angular.module("app", ["firebase"]);
 app.controller("MarketplaceCtrl", function($scope, $firebase) {
   var ref = new Firebase("https://8tracks-pebble.firebaseio.com/marketplace");
   var sync = $firebase(ref);
-  // create a synchronized array for use in our HTML code
   $scope.apps = sync.$asArray();
-  // $scope.hello = 'hi';
+
+  $scope.addApp = cats;
 });
+
+var cats = function(app) {
+  var currApp = app.$id;
+
+  var uid = localStorage.getItem("uid");
+
+  console.log(currApp);
+
+  var fb = new Firebase('https://8tracks-pebble.firebaseio.com/');
+
+  fb.child('marketplace').child(currApp).once('value', function(snapshot) {
+    var appCopy = snapshot.val();
+    fb.child('codes').child(uid).child(currApp).update(appCopy);
+    fb.child('codes').child(uid).child(currApp).child('img').remove();
+  });
+
+};
 
